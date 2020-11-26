9ae10fdf6c0cb3c82ab68e361b190be664fe7dd5
sander souto <sander.soares@yahoo.com.br>
2020-11-26 20:58:00 -0300

    Rails Task Manager challenge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,6 +42,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
